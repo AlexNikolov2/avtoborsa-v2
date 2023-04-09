647dece2d80e72fff786ebe259aad8df1c51ed0d
Aleks Nikolov <tcr.official.world@gmail.com>
2023-04-09 23:36:21 +0300

    added landing page

---
+++ avtoborsa-v2/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './LandingPage.css';
+
+const LandingPage = () => (
+  <div className="LandingPage" data-testid="LandingPage">
+    LandingPage Component
+  </div>
+);
+
+LandingPage.propTypes = {};
+
+LandingPage.defaultProps = {};
+
+export default LandingPage;
