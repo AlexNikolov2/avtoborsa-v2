bf63113a4728665997eb67d0d69477b8ab2a967e
Aleks Nikolov <tcr.official.world@gmail.com>
2023-04-13 20:00:00 +0300

    used cra, it isn't shit like vite

--- avtoborsa-v2/src/pages/LandingPage/LandingPage.jsx
+++ avtoborsa-v2/src/pages/LandingPage/LandingPage.jsx
@@ -1,32 +1,13 @@
 import React from "react";
 import "./LandingPage.css";
 
-const LandingPage = () => {
-  return (
-    <section className="LandingPage">
-      <h1 className="landing-up">Yet to find the car you want?</h1>
-      <h1 className="landing-center">A V T O B O R S A</h1>
-      <p className="landing-bottom">
-        Avtoborsa is the place to find the car of your dreams. Find, buy and
-        drive!
-      </p>
-      <ul>
-        <li>
-          <i className="fa-solid fa-car-side"></i>Lots of cars
-        </li>
-        <li>
-          <i className="fa-solid fa-clock"></i>Time Effecient
-        </li>
-        <li>
-          <i className="fa-solid fa-universal-access"></i>Easily accessible
-        </li>
-      </ul>
-      <a href="/search">
-        Find your car now <i className="fa-solid fa-magnifying-glass"></i>
-      </a>
-    </section>
-  );
-};
+const LandingPage = () => (
+  <div className="LandingPage" data-testid="LandingPage">
+    LandingPage Component
+  </div>
+);
+
+LandingPage.propTypes = {};
 
 LandingPage.defaultProps = {};
 
