8c6b0a0af252d71aa6df5b6c34412956fef92335
Aleks Nikolov <tcr.official.world@gmail.com>
2023-04-13 21:55:34 +0300

    jsx fix

---
+++ avtoborsa-v2/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <div className="Footer" data-testid="Footer">
+    Footer Component
+  </div>
+);
+
+Footer.propTypes = {};
+
+Footer.defaultProps = {};
+
+export default Footer;
