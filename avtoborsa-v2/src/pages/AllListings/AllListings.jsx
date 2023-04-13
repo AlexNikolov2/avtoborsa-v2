0e9a6bd4ec5a825ebf2c0a6b65104c782184f79d
Aleks Nikolov <tcr.official.world@gmail.com>
2023-04-13 21:48:00 +0300

    jsx

---
+++ avtoborsa-v2/src/pages/AllListings/AllListings.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./AllListings.css";
+
+const AllListings = () => (
+  <div className="AllListings" data-testid="AllListings">
+    AllListings Component
+  </div>
+);
+
+AllListings.propTypes = {};
+
+AllListings.defaultProps = {};
+
+export default AllListings;
