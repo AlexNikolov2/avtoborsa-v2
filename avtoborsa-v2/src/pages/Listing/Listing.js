47c1a05148c00c6ea91bd0aff80d8ad03db7adad
Aleks Nikolov <tcr.official.world@gmail.com>
2023-04-13 22:24:26 +0300

    listing component

---
+++ avtoborsa-v2/src/pages/Listing/Listing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Listing.css';
+
+const Listing = () => (
+  <div className="Listing" data-testid="Listing">
+    Listing Component
+  </div>
+);
+
+Listing.propTypes = {};
+
+Listing.defaultProps = {};
+
+export default Listing;
