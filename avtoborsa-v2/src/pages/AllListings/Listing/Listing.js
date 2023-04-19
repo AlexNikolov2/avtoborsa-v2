52be5dc8b483896bbce4918766eb84af6cf1de89
Aleks Nikolov <tcr.official.world@gmail.com>
2023-04-19 21:56:28 +0300

    listing to all listinfs

---
+++ avtoborsa-v2/src/pages/AllListings/Listing/Listing.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Listing.css";
+
+const Listing = () => (
+  <div className="Listing" data-testid="Listing">
+    Listing Component
+  </div>
+);
+
+Listing.propTypes = {};
+
+Listing.defaultProps = {};
+
+export default Listing;
